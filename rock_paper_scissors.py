17caedd6f6177e6bf7d6735e3d7cdcdf2af3ca87
Sikandar <sikandarbutt@hotmail.co.uk>
2023-10-15 10:47:58 +0100

    While loop for a user input and check if user typed correct string that is in the list.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -4,3 +4,13 @@ import random
 #2 variables for tracking scores, 1 for user and 1 for computer
 user_wins = 0
 computer_wins = 0
+
+#adding a while loop
+while True:
+    user_input =  input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        quit()
+
+    if user_input in ["Rock","Paper","Scissors"]:
+
+
