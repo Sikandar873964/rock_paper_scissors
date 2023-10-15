1a945c6d953ba632340324a11cbfda9d12b02333
Sikandar <sikandarbutt@hotmail.co.uk>
2023-10-15 14:37:41 +0100

    added computer pick from options

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,12 +5,27 @@ import random
 user_wins = 0
 computer_wins = 0
 
+options = ["Rock","Paper","Scissors"]
+#data structures, elements that are stored in the lists. 
+
 #adding a while loop
 while True:
     user_input =  input("Type Rock/Paper/Scissors or Q to quit: ").lower()
     if user_input == "q":
-        quit()
+        break #instead of quit, it will 'break' out of the while loop 
 
-    if user_input in ["Rock","Paper","Scissors"]:
+    #instead of 'in' we'll use 'not in' to reverse it, so if user types in something that is not in the list, user will then have to type something valid.  
+    if user_input not in ["Rock","Paper","Scissors"]:
+        continue
+
+    random_number = random.randint(0,2)
+    # Rock: 0, paper: 1, scissor: 2
+    computer_pick = options[random_number]
+    print("computer picked", computer_pick + ".")
+
+
+
+
+print("Goodbye")
 
 
